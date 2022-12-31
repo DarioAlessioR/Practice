b3f1a298a88b6a38241babc5b9b6a6b33816ad94
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-12-30 22:25:39 -0400

    Update rubytest2.rb

--- rubytest2.rb
+++ rubytest2.rb
@@ -1,3 +1,13 @@
+ # Author:  David M. C. Brown
+    # Date:  2015-10-01
+    # Description:  Ruby test 2
+    #
+    # 1. Write a method that takes a hash as an argument and returns a hash with the same keys and values but with the following changes:
+    #
+    # If the value is a string, capitalize it.
+    # If the value is an array, capitalize the first letter of each string in the array.
+    # If the value is a hash, capitalize the first letter of each key in the hash.
+ 
  def computeClosestToZero
     closest = nil
     for i in 0..(args.length-1)
