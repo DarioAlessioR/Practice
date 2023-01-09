674630604b65592a1417c6f886bb5b2791104422
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-01-09 16:13:05 -0400

    Create rubytest7.rb

---
+++ rubytest7.rb
@@ -0,0 +1,24 @@
+tools = ["ruby", "rspec", "rails"]
+# Return true if every element of the tools array starts with an "r" and false otherwise.
+
+# Solution 1: 
+result = []
+tools.each do |tool|
+    if tool[0] == "r"
+    result << tool[0]
+    end
+end
+
+if result.length == tools.length
+  puts true
+else
+  puts false
+end
+
+
+# Solution 2: it does not print in the console
+
+tools.all? do |tool|
+  tool[0] == "r"
+end
+
