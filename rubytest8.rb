e4850806e84d37fb2e73d641d5bd68e0383e4d3c
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-01-09 16:18:02 -0400

    Create rubytest8.rb

---
+++ rubytest8.rb
@@ -0,0 +1,19 @@
+# Return true if the string "stimpy" contains the letter "s" and false otherwise.
+
+# Solution 1: it works
+stimpy = "stimpy"
+ 
+if stimpy.include?("s")
+  puts true
+else
+  puts false
+end
+
+# Solution 2: it does not print in the console
+ stimpy = "stimpy"
+ 
+ def has_s?(string)
+   string.include?("s")
+ end
+ 
+ has_s?(stimpy)
